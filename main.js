ec2681828b295bfc5d42ebe694a8cb8a122bdd85
Victor <vcmikhaelcp@gmail.com>
2023-07-07 11:45:48 -0300

    adicionando validação de e-mail

--- main.js
+++ main.js
@@ -3,16 +3,23 @@ const card = document.getElementById('card')
 const card2 = document.getElementById('card-2')
 const dismiss = document.getElementById('dismiss')
 const input = document.getElementById('email')
+const error = document.getElementById('valid-error')
+
 
 campoEmail.addEventListener('submit', (event) => {
     event.preventDefault()
-    card.classList.add('hide')
-    card2.classList.remove('hide')
-    const validacao = email.value
+    const validacao = input.value
     const textoEmail = document.querySelector('.texto-email')
+    
+    if(validacaoEmail(input.value) !== true){
+        error.classList.remove('hide')
+        input.classList.add('error')
+    } else {
+    card.classList.add('hide')
+    card2.classList.remove('hide')   
     textoEmail.innerHTML = `
     <p class="conteudo-texto texto-2">A confirmation email has been sent to <span>${validacao}</span>. Please open it and click the button inside to confirm your subscription.</p>
-    `
+    `}
 })
 
 dismiss.addEventListener('click', (event) => {
@@ -21,4 +28,8 @@ dismiss.addEventListener('click', (event) => {
     card2.classList.add('hide')
 })
 
+function validacaoEmail(input) {
+    let emailRegex = /^[a-zA-Z0-9][a-zA-Z0-9\._-]+@([a-zA-Z0-9\._-]+\.)[a-zA-Z-0-9]{2,3}/;
+    return emailRegex.test(input)
+}
 
